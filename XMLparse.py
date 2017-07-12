2fd7330319db23f913af0b19c5be5d8ee7d96112
Brian Tilley <briantilley97@gmail.com>
2017-07-12 16:12:20 -0500

    strict searching, progress

--- XMLparse.py
+++ XMLparse.py
@@ -93,7 +93,6 @@ def attribute_subset_match(source_node, target_node):
 
 # match root-level structure
 def match_structures(source_root, target_root):
-
 	return (source_root.tag == target_root.tag) and attribute_subset_match(source_root, target_root) and setwise_match(source_root, target_root)
 
 # return a list of all first-order children of each element in the provided list
@@ -125,14 +124,56 @@ def exhaustive_match(source_root, target_root, mode="bfs"):
 
 	return False
 
-# (breadth-first or depth-first) return the roots of all structures matching the target
-def exhaustive_search(source_root, target_root, mode= "dfs", match_list=[]):
+# remove nodes from source not present in target
+def trim_branches(matched_root, target_root):
+
+	# copy the top level of matched_root only
+	trimmed_root = et.Element(matched_root.tag, matched_root.attrib)
+	trimmed_root.text = matched_root.text
+	# note: matched_root.tail is not used elsewhere, so it is ignored
+
+	# (base case) target_root has no children
+	if len(target_root) == 0:
+		return trimmed_root
+
+	# copy matched_root's children into a list
+	matched_children = []
+	for i in range(0, len(matched_root)):
+		matched_children.append(matched_root[i])
+
+	# remove extra children from beginning/middle of match
+	for t_child_index in range(0, len(target_root)):
+		while not match_structures(matched_children[t_child_index], target_root[t_child_index]):
+			matched_children.pop(t_child_index)
+
+	# remove extra children from end of match
+	diff = max(0, len(matched_children) - len(target_root))
+	for i in range(0, diff):
+		matched_children.pop()
+
+	# (recursion) trim each child and add it back to the root
+	for i in range(0, len(matched_children)):
+		trimmed_root.append(trim_branches(matched_children[i], target_root[i]))
+
+	return trimmed_root
+
+# (breadth-first or depth-first) return the root of first-found structure matching the target
+# strict: trim match to remove untargeted elements
+def search(source_root, target_root, strict=False, mode= "dfs"):
 	if mode == "dfs":
 		if match_structures(source_root, target_root):
-			match_list.append(source_root)
+			if strict:
+				return trim_branches(source_root, target_root)
+			else:
+				return source_root
 
 		for child in source_root:
-			exhaustive_search(child, target_root, "dfs", match_list)
+			r = search(child, target_root, "dfs")
+			if r:
+				if strict:
+					return(trim_branches(r, target_root))
+				else:
+					return r
 
 	elif mode == "bfs":
 		parent_list = list(source_root)
@@ -140,7 +181,37 @@ def exhaustive_search(source_root, target_root, mode= "dfs", match_list=[]):
 		while len(parent_list) > 0:
 			for parent in parent_list:
 				if match_structures(parent, target_root):
-					match_list.append(parent)
+					if strict:
+						return trim_branches(parent, target_root)
+					else:
+						return parent
+
+			parent_list = one_level_in(parent_list)
+
+	return None
+
+# (breadth-first or depth-first) return the roots of all structures matching the target
+def exhaustive_search(source_root, target_root, strict=False, mode= "dfs", match_list=[]):
+	if mode == "dfs":
+		if match_structures(source_root, target_root):
+			if strict:
+				match_list.append(trim_branches(source_root, target_root))
+			else:
+				match_list.append(source_root)
+
+		for child in source_root:
+			exhaustive_search(child, target_root, strict, "dfs", match_list)
+
+	elif mode == "bfs":
+		parent_list = list(source_root)
+
+		while len(parent_list) > 0:
+			for parent in parent_list:
+				if match_structures(parent, target_root):
+					if strict:
+						match_list.append(trim_branches(parent, target_root))
+					else:
+						match_list.append(parent)
 
 			parent_list = one_level_in(parent_list)
 
@@ -162,7 +233,7 @@ def show_tree(root):
 def get_root_from_arg(arg):
 	if re.match(".*[.].*", arg):
 		return et.parse(arg).getroot()
-	return et.fromstring(arg)
+	return et.XML(arg)
 
 # return the XML text equivalent of a tree
 def get_source(node):
@@ -172,16 +243,25 @@ def get_source(node):
 
 if __name__ == "__main__":
 
-	# usage check
-	if len(sys.argv) != 3:
-		print("usage:", sys.argv[0], "source_XML target_structure")
-		sys.exit(1)
+	# # usage check
+	# if len(sys.argv) != 3:
+	# 	print("usage:", sys.argv[0], "source_XML target_structure")
+	# 	sys.exit(1)
 
-	# obtain XML trees
-	source_root = get_root_from_arg(sys.argv[1])
-	target_root = get_root_from_arg(sys.argv[2])
+	# # obtain XML trees
+	# source_root = get_root_from_arg(sys.argv[1])
+	# target_root = get_root_from_arg(sys.argv[2])
 
-	# obtain all matches present, then print each on its own line
-	matches = exhaustive_search(source_root, target_root)
-	for match in matches:
-		print(get_source(match))
+	# # obtain all matches present, then print each on its own line
+	# matches = exhaustive_search(source_root, target_root)
+	# for match in matches:
+	# 	print(get_source(match))
+
+	source_root = et.XML("<div id=\"gallery\"><q></q><w></w><e></e><div id=\"browse\"><r></r><h1><l></l><z></z><x></x><c></c><v></v><b></b><n></n><m></m></h1><t></t></div><y></y><u></u><div id=\"projectImage\"><i></i><o></o><p></p><a></a><img class=\"product-img\"/><s></s><d></d><f></f></div><g></g><h></h><j></j><k></k></div>")
+	target_root = et.XML("<div id=\"gallery\"><div id=\"browse\"><h1></h1></div><div id=\"projectImage\"><img class=\"product-img\"/></div></div>")
+	match_root  = search(source_root, target_root)
+	print(get_source(target_root))
+	print()
+	print(get_source(match_root))
+	print()
+	print(get_source(trim_branches(match_root, target_root)))
